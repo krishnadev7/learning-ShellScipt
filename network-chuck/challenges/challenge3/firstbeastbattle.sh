b5ded4df0feaf474c5336d214273e464b325c343
krishnadev7 <krishnadevv12@gmail.com>
2023-05-22 20:10:51 +0530

    completed boss battle

--- network-chuck/challenges/challenge3/firstbeastbattle.sh
+++ network-chuck/challenges/challenge3/firstbeastbattle.sh
@@ -10,4 +10,20 @@ if [[ $beast == $number ]]; then
 	echo "Beast VANQUISHED!! Congrats!!!"
 else 
 	echo "You Died"
+	exit 1
 fi
+
+sleep 2
+
+beast=$(( $RANDOM % 2 ))
+
+echo "Boss battle. Get scared. It's Margit. Pick a number between 0-9. (0-9)"
+
+read number;
+
+if [[ $beast == $number || $number == "please help me" ]]; then
+	echo "Beast VANQUISHED!! Congrats"
+else
+	echo "You Died"
+fi
+
